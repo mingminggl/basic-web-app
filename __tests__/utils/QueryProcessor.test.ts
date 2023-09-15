3c574ce7d1831d3dee3882a97bdee1eb2d5e94c2
mingminggl <88123825+mingminggl@users.noreply.github.com>
2023-09-15 17:03:50 -0400

    Added andrew id to query processor

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -17,4 +17,13 @@ describe("QueryProcessor", () => {
             "writer in the English language and the world's pre-eminent dramatist."
           ));
     });
+
+    test('should return andrew id', () => {
+        const query = "What is your Andrew ID?";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "Your Andrew id is " +
+            "abcdef."
+          ));
+    });
 });
